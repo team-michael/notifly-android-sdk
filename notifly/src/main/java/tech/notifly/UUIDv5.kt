b2b9c5fee0d4a1d7dcc169e17e0f3c861d1f073b
pjhjohn <pjhjohn@gmail.com>
2023-04-11 19:16:11 +0900

    Implement UUID V5 Generator
    Tested with Namespace "3e377350-462d-4108-ab5a-aa71146380d2" from site https://www.sohamkamani.com/uuid-versions-explained/
    
    asdf
    reference: 4dbaa502-4a7e-5f81-b08a-775e5594288a
    notifly:   4dbaa502-4a7e-5f81-b08a-775e5594288a
    
    qwer
    reference: d67ce1f9-5311-565f-9103-5d01efb02825
    notifly:   d67ce1f9-5311-565f-9103-5d01efb02825
    
    zxcv
    reference: ad6e4515-83a2-5656-9f9f-106ca3ec92d3
    notifly:   ad6e4515-83a2-5656-9f9f-106ca3ec92d3
    
    1234
    reference: 96452ea4-24a3-5fc2-a8cc-a47f0ee27d48
    notifly:   96452ea4-24a3-5fc2-a8cc-a47f0ee27d48

---
+++ notifly/src/main/java/tech/notifly/UUIDv5.kt
@@ -0,0 +1,41 @@
+package tech.notifly
+
+import java.nio.ByteBuffer
+import java.security.MessageDigest
+import java.util.UUID
+
+object UUIDv5 {
+
+    enum class Namespace(val uuid: UUID) {
+        NAMESPACE_EVENT_ID(UUID.fromString("830b5f7b-e392-43db-a17b-d835f0bcab2b")),
+        NAMESPACE_REGISTERED_USER_ID(UUID.fromString("ce7c62f9-e8ae-4009-8fd6-468e9581fa21")),
+        NAMESPACE_UNREGISTERED_USER_ID(UUID.fromString("a6446dcf-c057-4de7-a360-56af8659d52f")),
+        NAMESPACE_DEVICE_ID(UUID.fromString("830848b3-2444-467d-9cd8-3430d2738c57")),
+    }
+
+    fun generate(namespace: Namespace, name: String): UUID {
+        val namespaceBytes = ByteBuffer.wrap(ByteArray(16))
+            .putLong(namespace.uuid.mostSignificantBits)
+            .putLong(namespace.uuid.leastSignificantBits)
+            .array()
+
+        val nameBytes = name.toByteArray(Charsets.UTF_8)
+
+        val inputData = namespaceBytes + nameBytes
+
+        val digest = MessageDigest.getInstance("SHA-1")
+        val uuidBytes = digest.digest(inputData).copyOfRange(0, 16)
+
+        uuidBytes[6] = (uuidBytes[6].toInt() and 0x0F).toByte()
+        uuidBytes[6] = (uuidBytes[6].toInt() or (5 shl 4)).toByte()
+
+        uuidBytes[8] = (uuidBytes[8].toInt() and 0x3F).toByte()
+        uuidBytes[8] = (uuidBytes[8].toInt() or 0x80).toByte()
+
+        val byteBuffer = ByteBuffer.wrap(uuidBytes)
+        val mostSignificantBits = byteBuffer.long
+        val leastSignificantBits = byteBuffer.long
+
+        return UUID(mostSignificantBits, leastSignificantBits)
+    }
+}
