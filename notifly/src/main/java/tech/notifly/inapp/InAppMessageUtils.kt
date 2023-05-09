9def4812e1b074f9d939a2894a80548a2897f286
minyonglee <rickylee87@gmail.com>
2023-05-09 17:46:56 +0900

    in app message modal size - work in progress

---
+++ notifly/src/main/java/tech/notifly/inapp/InAppMessageUtils.kt
@@ -0,0 +1,73 @@
+package tech.notifly.inapp
+
+import android.app.Activity
+import android.os.Build
+import android.util.DisplayMetrics
+import android.view.WindowInsets
+import org.json.JSONObject
+
+object InAppMessageUtils {
+
+    fun getViewDimensions(
+        modalProps: JSONObject?,
+        screenWidth: Int,
+        screenHeight: Int
+    ): Pair<Float, Float> {
+
+        val viewWidth: Float = when {
+            modalProps == null -> screenWidth.toFloat()
+            modalProps.has("width") -> modalProps.getInt("width").toFloat()
+            modalProps.has("width_vw") -> screenWidth * (modalProps.getInt("width_vw") / 100f)
+            modalProps.has("width_vh") -> screenHeight * (modalProps.getInt("width_vh") / 100f)
+            else -> screenWidth.toFloat()
+        }
+
+        val viewHeight: Float = when {
+            modalProps == null -> screenHeight.toFloat()
+            modalProps.has("height") -> modalProps.getInt("height").toFloat()
+            modalProps.has("height_vh") -> screenHeight * (modalProps.getInt("height_vh") / 100f)
+            modalProps.has("height_vw") -> screenWidth * (modalProps.getInt("height_vw") / 100f)
+            else -> screenHeight.toFloat()
+        }
+
+        val minWidth = getNullableIntProperty(modalProps, "min_width")
+        val maxWidth = getNullableIntProperty(modalProps, "max_width")
+        val minHeight = getNullableIntProperty(modalProps, "min_height")
+        val maxHeight = getNullableIntProperty(modalProps, "max_height")
+
+        return Pair(
+            getViewDimensionWithLimits(viewWidth, minWidth, maxWidth),
+            getViewDimensionWithLimits(viewHeight, minHeight, maxHeight)
+        )
+    }
+
+    private fun getNullableIntProperty(props: JSONObject?, name: String): Int? {
+        return props?.let {
+            if (it.has(name)) it.getInt(name) else null
+        }
+    }
+
+    private fun getViewDimensionWithLimits(dimension: Float, min: Int?, max: Int?): Float {
+        return when {
+            min != null && dimension < min -> min.toFloat()
+            max != null && dimension > max -> max.toFloat()
+            else -> dimension
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    fun getScreenWidthAndHeight(activity: Activity): Pair<Int, Int> {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = activity.windowManager.currentWindowMetrics
+            val insets =
+                windowMetrics.windowInsets.getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            val screenWidth = windowMetrics.bounds.width() - insets.left - insets.right
+            val screenHeight = windowMetrics.bounds.height() - insets.top - insets.bottom
+            Pair(screenWidth, screenHeight)
+        } else {
+            val displayMetrics = DisplayMetrics()
+            activity.windowManager.defaultDisplay.getMetrics(displayMetrics)
+            Pair(displayMetrics.widthPixels, displayMetrics.heightPixels)
+        }
+    }
+}
--- notifly/src/main/java/tech/notifly/inapp/NotiflyInAppMessageActivity.kt
+++ notifly/src/main/java/tech/notifly/inapp/NotiflyInAppMessageActivity.kt
@@ -3,7 +3,6 @@ package tech.notifly.inapp
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.graphics.Point
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -12,11 +11,11 @@ import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.widget.FrameLayout
 import androidx.appcompat.app.AlertDialog
+import org.json.JSONObject
 import tech.notifly.Notifly
 import tech.notifly.R
-import tech.notifly.storage.NotiflyStorage
-import tech.notifly.storage.NotiflyStorageItem
 import tech.notifly.utils.NotiflyLogUtil
+import kotlin.math.roundToInt
 
 class NotiflyInAppMessageActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +28,32 @@ class NotiflyInAppMessageActivity : Activity() {
         val campaignId = intent.getStringExtra("in_app_message_campaign_id")!!
         val url = intent.getStringExtra("in_app_message_url")
         val notiflyMessageId = intent.getStringExtra("notifly_message_id")
+        val modalPropertiesString = intent.getStringExtra("modal_properties")
+        val modalProperties = try {
+            if (modalPropertiesString != null) {
+                JSONObject(modalPropertiesString)
+            } else {
+                null
+            }
+        } catch (e: Exception) {
+            Log.e(Notifly.TAG, "Error parsing modal properties of the in app message", e)
+            null
+        }
+
+        val (screenWidth, screenHeight) = InAppMessageUtils.getScreenWidthAndHeight(this)
+        val (widthPx, heightPx) = InAppMessageUtils.getViewDimensions(
+            modalProperties,
+            screenWidth,
+            screenHeight
+        )
+        Log.d(Notifly.TAG, "screenWidth: $screenWidth, screenHeight: $screenHeight")
+        Log.d(Notifly.TAG, "In-app message widthPx: $widthPx, heightPx: $heightPx")
+
+        modalProperties?.let { properties ->
+            val position = properties.optString("position", "full")
+            setPositionAndSize(webView, widthPx, heightPx, position)
+        }
+
         url?.let {
             webView.loadUrl(it)
             NotiflyLogUtil.logEvent(
@@ -42,8 +67,31 @@ class NotiflyInAppMessageActivity : Activity() {
                 ),
                 listOf(),
                 true
-            ); // logging in app messaging delivered
+            ) // logging in app messaging delivered
         }
+    }
+
+    private fun setPositionAndSize(
+        webView: WebView,
+        widthPx: Float,
+        heightPx: Float,
+        position: String
+    ) {
+        val layoutParams = FrameLayout.LayoutParams(widthPx.roundToInt(), heightPx.roundToInt())
+
+        when (position) {
+            "top" -> layoutParams.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+            "bottom" -> layoutParams.gravity = Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL
+            "left" -> layoutParams.gravity = Gravity.START or Gravity.CENTER_VERTICAL
+            "right" -> layoutParams.gravity = Gravity.END or Gravity.CENTER_VERTICAL
+            "center" -> layoutParams.gravity = Gravity.CENTER
+            else -> { // Default to full screen
+                layoutParams.width = FrameLayout.LayoutParams.MATCH_PARENT
+                layoutParams.height = FrameLayout.LayoutParams.MATCH_PARENT
+            }
+        }
+
+        webView.layoutParams = layoutParams
     }
 
 
@@ -59,6 +107,7 @@ class NotiflyInAppMessageActivity : Activity() {
                         (webView.parent.parent as? AlertDialog)?.dismiss()
                     }
                 }
+
                 "main_button" -> {
                     if (link != null) {
                         (context as Activity).runOnUiThread {
