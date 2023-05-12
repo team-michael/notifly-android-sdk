3a72cab77af3e59c5788cf229ad312681d183a04
minyonglee <rickylee87@gmail.com>
2023-05-12 13:37:58 +0900

    version 0.0.1

--- notifly/src/main/java/tech/notifly/Notifly.kt
+++ notifly/src/main/java/tech/notifly/Notifly.kt
@@ -14,7 +14,7 @@ import tech.notifly.utils.NotiflyUserUtil
 
 object Notifly {
     internal const val TAG = "Notifly"
-    internal const val VERSION = "1.0.0"
+    internal const val VERSION = "0.0.1"
     internal val SDK_TYPE = NotiflySdkType.NATIVE
     internal const val NOTIFICATION_CHANNEL_ID = "NotiflyNotificationChannelId"
 
