2ad6f7cc98f5f87dd65b432c3abd9a179ae02ac7
csjunha <qkrwnsgk0303@kaist.ac.kr>
2023-06-26 22:35:28 +0900

    Fix authentication endpoint

--- notifly/src/main/java/tech/notifly/utils/NotiflyAuthUtil.kt
+++ notifly/src/main/java/tech/notifly/utils/NotiflyAuthUtil.kt
@@ -16,8 +16,7 @@ import kotlin.jvm.Throws
 
 internal object NotiflyAuthUtil {
 
-    private const val AUTHENTICATOR_URL = "https://cognito-idp.ap-northeast-2.amazonaws.com"
-    private const val AUTHENTICATOR_CLIENT_ID = "2pc5pce21ec53csf8chafknqve"
+    private const val AUTHENTICATOR_URL = "https://api.notifly.tech/authorize"
 
     /**
      * Retrieves CognitoIdToken with given [username] and [password].
@@ -29,26 +28,20 @@ internal object NotiflyAuthUtil {
     @Throws(NullPointerException::class)
     suspend fun getCognitoIdToken(username: String, password: String): String {
         val requestBody = JSONObject().apply {
-            put("AuthFlow", "USER_PASSWORD_AUTH")
-            put("AuthParameters", JSONObject().apply {
-                put("USERNAME", username)
-                put("PASSWORD", password)
-            })
-            put("ClientId", AUTHENTICATOR_CLIENT_ID)
+            put("userName", username)
+            put("password", password)
         }
 
         val request = Request.Builder()
             .url(AUTHENTICATOR_URL)
-            .addHeader("X-Amz-Target", "AWSCognitoIdentityProviderService.InitiateAuth")
-            .post(requestBody.toString().toRequestBody("application/x-amz-json-1.1".toMediaType()))
+            .post(requestBody.toString().toRequestBody("application/json".toMediaType()))
             .build()
 
         return withContext(Dispatchers.IO) {
             try {
                 val response = N.HTTP_CLIENT.await(request)
                 val jsonResponse = JSONObject(response.body!!.string())
-                val authenticationResult = jsonResponse.getJSONObject("AuthenticationResult")
-                authenticationResult.getString("IdToken")
+                jsonResponse.getString("data")
             } catch (e: Exception) {
                 Logger.e("Authentication Failed", e)
                 throw NullPointerException("Failed to Get Cognito ID Token")
@@ -69,13 +62,15 @@ internal object NotiflyAuthUtil {
         // retrieve and put new user-id
         val projectId: String = NotiflyStorage.get(context, NotiflyStorageItem.PROJECT_ID)
             ?: throw IllegalStateException("[Notifly] <Project ID> not found. You should call Notifly.initialize first")
-        val externalUserId: String? = NotiflyStorage.get(context, NotiflyStorageItem.EXTERNAL_USER_ID)
+        val externalUserId: String? =
+            NotiflyStorage.get(context, NotiflyStorageItem.EXTERNAL_USER_ID)
 
         val notiflyUserId = when {
             externalUserId != null -> NotiflyIdUtil.generate(
                 NotiflyIdUtil.Namespace.NAMESPACE_REGISTERED_USER_ID,
                 "${projectId}${externalUserId}"
             )
+
             else -> NotiflyIdUtil.generate(
                 NotiflyIdUtil.Namespace.NAMESPACE_UNREGISTERED_USER_ID,
                 "${projectId}${NotiflyFirebaseUtil.getFcmToken()}"
