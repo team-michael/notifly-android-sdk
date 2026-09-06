9e41f4961ab917d4d9e4024b8b574ee68e591d81
박건우 <gunwoo@baggeon-uui-MacBookPro-2.local>
2026-09-06 15:55:09 +0900

    feat: use shared KMP user transition policy

--- notifly/build.gradle.kts
+++ notifly/build.gradle.kts
@@ -59,6 +59,7 @@ android {
 }
 
 dependencies {
+    implementation("com.github.team-michael.notifly-kmp-sdk:kmp:v0.1.0-alpha.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
 
