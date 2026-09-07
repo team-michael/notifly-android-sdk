3c61539fe7185ece3cb1b585f054e73029526fa2
박건우 <gunwoo@baggeon-uui-MacBookPro-2.local>
2026-09-07 18:15:15 +0900

    chore: bump notifly-kmp-sdk to v0.1.0-alpha.3

--- notifly/build.gradle.kts
+++ notifly/build.gradle.kts
@@ -59,7 +59,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.notifly-tech.notifly-kmp-sdk:kmp:v0.1.0-alpha.2")
+    implementation("com.github.notifly-tech.notifly-kmp-sdk:kmp:v0.1.0-alpha.3")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
 
